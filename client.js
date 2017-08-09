1eaab33079a2b389a92b70787c75af3a76decafb
BKova <bartul3.kovacic3@gmail.com>
2017-08-09 18:00:56 +0200

    Implement DEBUG log
    * Implement debug log for client, saml and all http requests

--- client.js
+++ client.js
@@ -1,6 +1,8 @@
 const urlJoin = require('url-join');
 const endOfToday = require('date-fns/end_of_today');
 const addDays = require('date-fns/add_days');
+const debugClient = require('debug')('client');
+const debugHttp = require('debug')('http');
 const isWithinRange = require('date-fns/is_within_range');
 const r = require('./lib/request');
 const { samlRequest, samlResponse } = require('./lib/saml');
@@ -20,7 +22,7 @@ class Client {
       username,
       password,
     };
-
+    debugClient('Started login');
     const url = urlJoin(this.baseUrl, '/isspaaieduhr/login.ashx');
     return initAuthAction(url)
       .then(html => samlRequest(r, html))
@@ -33,37 +35,45 @@ class Client {
         }
         const user = parseUserInfo(data);
         Object.assign(this.user, getUserInfo(html), user);
+        debugClient('Finished login');
         return this;
       });
   }
 
   getRecipes(dayLimit) {
+    debugClient('Getting all recipes');
     const url = urlJoin(this.baseUrl, '/StudentRacun');
     const options = {
       qs: { oib: this.user.oib, jmbag: this.user.jmbag },
     };
+    debugHttp('GET %o', url);
     return r.get(url, options)
       .then(([, html]) => getRecipes(html))
       .then(recipes => filter(recipes, recipe => limitDate(recipe.time, dayLimit)));
   }
 
   getRecipeDetails(recipe) {
+    debugClient('Getting recipe %o', recipe.id);
     const url = urlJoin(this.baseUrl, '/StudentRacun/RacunDetalji');
     const options = { json: recipe.id };
+    debugHttp('POST %o', url);
     return r.post(url, options)
       .then(([, html]) => getRecipeDetails(html));
   }
 
   logout() {
     const url = urlJoin(this.baseUrl, '/KorisnickiRacun/Odjava');
+    debugClient('Started logout');
     return initAuthAction(url)
-      .then(html => samlRequest(r, html));
+      .then(html => samlRequest(r, html))
+      .then(() => debugClient('Finished logout'));
   }
 }
 
 module.exports = Client;
 
 function initAuthAction(url) {
+  debugHttp('GET %o', url);
   return r.get(url)
     .then(([, html]) => html);
 }
@@ -77,6 +87,7 @@ function doLogin(url, credentials, authState) {
       AuthState: authState,
     },
   };
+  debugHttp('POST %o', url);
   return r.post(url, options)
     .then(([, html]) => html);
 }
--- example.js
+++ example.js
@@ -5,9 +5,9 @@ const client = new Client();
 
 client.login(credentials.username, credentials.password)
   .then(() => client.getRecipes(360))
-  .then((recipes) => {
-    console.log(recipes);
-    return client.getRecipeDetails(recipes[0]);
-  })
-  .then(html => console.log(html))
+  .then(recipes =>
+  // onsole.log(recipes);
+    client.getRecipeDetails(recipes[0]),
+  )
+  // .then(html => console.log(html))
   .then(() => client.logout());
